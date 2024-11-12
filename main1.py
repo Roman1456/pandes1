af6f0633b3984cf1e9375ed1a935b7ccbc5f1840
Lpopivnyak <stadnykroman2010@gmail.com>
2024-11-12 16:09:21 +0200

    bgffgfffvff

--- main1.py
+++ main1.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('Space_Corrected (1).csv')
 df.info()
 
 a = df['Status Rocket'].value_counts()
-print(a)
+a.plot(kind='pie',color=['green','red'])
+
+plt.title("Кількість ракет за статусом")
+plt.show()
--- main2.py
+++ main2.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('Space_Corrected (1).csv')
-df.info()
-df[" Rocket"] = pd.to_numeric(df[" Rocket"], errors='coerce')
 
-df[" Rocket"].fillna(df[" Rocket"].mean(), inplace=True)
-print("Порожні значення замінено на середні значення")
-
-# Заміна коми на крапку в усіх значеннях DataFrame
-df[" Rocket"] = df[" Rocket"].replace('.', ',', regex=True)
 
 
